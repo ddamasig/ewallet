b909889e630c3a0c4e38a21b09fc865536bc13bf
Dean Simon R. Damasig <ddamasig@gmail.com>
2022-03-17 18:09:15 +0800

    feat: added JS files for deployment

---
+++ resources/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dev-easy-wallet',
+      script: './start.js',
+      env: {
+        HOST: 'localhost',
+        PORT: 3000
+      }
+    }
+  ]
+}
